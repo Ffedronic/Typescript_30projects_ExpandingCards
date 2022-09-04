6ddf9debae73f682e86ff829117db51e5a820f16
felix fedronic <fedronicfelix@gmail.com>
2022-09-04 17:58:09 +0200

    remise à zéro

--- components/GridExpandingCards.tsx
+++
@@ -1,9 +0,0 @@
-import ExpandingCard from "./ExpandingCard";
-
-function gridExpandingCards() {
-  return(
-    <ExpandingCard />
-  )
-}
-
-export default gridExpandingCards;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import GridExpandingCards from '../components/GridExpandingCards'
-
-import { backgroundImages } from "../helpers/expandingCards-util"
-
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Expanding Cards</title>
         <meta name="description" content="Expanding cards created from the Brad Traversy's example." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>HomePage</h1>
-      <GridExpandingCards />
     </div>
   )
 }
